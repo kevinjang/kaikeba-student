1f784504386e0646739fa8a0a3f437dc7bf16ff3
kevinjang <kevinjang@163.com>
2018-12-23 22:12:06 +0800

    first line

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,57 @@
 import { Component } from '@angular/core';
 
+// 变量、常量
+let name;
+const PI = 3.1415926;
+
+// 元组
+let x: [string, number] = ['hello', 10];
+
+// 任意类型
+let notSure : any;
+notSure = 4;
+notSure = 'jerry';
+
+let list: any[] = [1,true,'jerry'];
+list[1] = 100;
+
+// 枚举
+enum Color {
+  Red,
+  Green,
+  Blue
+}
+
+let c: Color = Color.Blue;
+
+console.log(c);
+console.log(Color[1]);
+
+// 函数的特殊用法
+function greeting(person: string) {
+  return 'Hello,' + person;
+}
+
+greeting('tom');
+
+// void类型 没有返回值
+function warnUser(): void {
+  alert('aaa');
+}
+
+// 接口
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+function greeting2(person: Person) {
+  return 'Hello, '+ person.firstName+ '-'+ person.lastName;
+}
+
+const gre = greeting2({firstName:'tom',lastName:'cruise'});
+console.log(gre);
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
